81f99bc4c248819a53f7a1f7df3bd72226a95af0
Sayon Dey <deysayon6@gmail.com>
2025-07-13 14:05:12 +0530

    Removed .env file and added to .gitignore

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,4 +1,5 @@
 import { inngest } from "@/config/inngest";
+import Order from "@/models/Order";
 import Product from "@/models/Product";
 import User from "@/models/User";
 import { getAuth } from "@clerk/nextjs/server";
@@ -20,15 +21,13 @@ export async function POST(request) {
             return await acc + product.offerPrice * item.quantity;
         },0)
 
-        await inngest.send({
-            name: 'order/created',
-            data: {
+        await Order.create({
                 userId,
                 address,
                 items,
                 amount: amount + Math.floor(amount * 0.02),
-                date: Date.now()
-            }
+                date: Date.now(),
+                paymentType: 'COD'
         })
 
         const user = await User.findById(userId)
